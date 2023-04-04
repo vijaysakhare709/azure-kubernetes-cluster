353b19953a92eeaa03d04ba59e70c32427631cc4
vijaysakhare709 <129182134+vijaysakhare709@users.noreply.github.com>
2023-04-04 13:41:35 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "resource_group_name" {
+    type = string
+    description  = "resource group name"
+}
+
+variable "location" {
+    type = string
+    description = "location of resource group"
+}
+
+variable "kubernetes-cluster-name" {
+    type = string
+}
